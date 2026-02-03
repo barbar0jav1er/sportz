078256438d39caaff6a471ec127575912e5b6073
Barbaro Javier <barbar0jav1er@callbackhell.run>
2026-02-03 18:12:28 +0400

    feat: update database schema and add commentary and matches tables

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.js",
+  schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
